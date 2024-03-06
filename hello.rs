c83f1afe3c6909c10eda692a91c8f23cfab25fe8
root <root@john-rehc-01.evoxt.com>
2024-03-06 08:26:00 +0100

    add git status

--- hello.rs
+++ hello.rs
@@ -11,5 +11,5 @@ fn main() {
     // Statements here are executed when the compiled binary is called.
 
     // Print text to the console.
-    println!("Hello World!");
+    println!("Hello World!!!");
 }
